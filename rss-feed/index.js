f9d8bbb2a2bd52419b9aed2e96157a1722b11799
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-01-29 17:11:43 -0600

    Installed Node and ran it on the port 4000.

---
+++ rss-feed/index.js
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import express from 'express';
+import Parser from 'rss-parser';
+
+const parser = new Parser();
+
+let articles = [];
+
+(async () => {
+    const feed = await parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
+    console.log(feed.title);
+
+    feed.items.forEach(item => {
+        articles.push([item]);
+    });
+})();
+
+let app = express();
+app.use(cors);
+
+app.get('/', (req,res) => {
+    res.send(articles);
+})
+
+const server = app.listen('4000', () => {
+    console.log("App is running at http://localhost:4000");
+})
+
+export default server;
