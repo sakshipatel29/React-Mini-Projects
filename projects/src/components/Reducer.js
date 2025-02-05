2772a4304c7cf8455b6b6632ddaeee94dfceab7a
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-04 18:43:58 -0600

    Used UseReduce Hook inplace of UseState Hook.

--- projects/src/components/Reducer.js
+++ projects/src/components/Reducer.js
@@ -1,46 +1,92 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+
+const logInReduce = (state, action ) => {
+    switch (action.type) {
+        case "field": {
+            return {
+            ...state,
+            [action.fieldName] : action.payload
+            }
+        }
+        case "logIn": {
+            return {
+                ...state,
+                error: "",
+            }
+        }
+        case "success":{
+            return{
+                ...state,
+                loggedIn: true,
+                password: "",
+            }
+        }
+        case "error": {
+            return{
+                ...state,
+                error: "Incorrect Credentials!",
+                loggedIn: false,
+                username: "",
+                password: "",
+            }
+        }
+        case "logOut": {
+            return{
+                ...state,
+                loggedIn: false,
+            }
+        }
+        default: return state; 
+    }
+}
 
 const Reducer = () => {
-
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [error, setError] = useState("");
+    const [state, dispatch] = useReducer(logInReduce, {
+        username: "",
+        password: "",
+        loggedIn: false,
+        error: "",
+    });
 
     const handleSumbit = e => {
         e.preventDefault();
-        setError("");
+        dispatch({type: "logIn"});
         try {
-            if (username === "Saifali" && password === "Ronak") {
-                setLoggedIn(true);
+            if (state.username === "Saifali" && state.password === "Ronak") {
+                dispatch({type: "success"});
             } else {
                 throw Error;
             }
-            setPassword("");
         } catch (error) {
-            setError("Incorrect Username or Password!");
-            setUsername("");
-            setPassword("");
+            dispatch({type: "error"});
         }
     }
     return (
         <div>
             <h2>Use Reducer Learning</h2>
             <div>
-                {loggedIn ? (
+                {state.loggedIn ? (
                     <>
-                        <h2>Welcome {username}!</h2>
-                        <button onClick={() => setLoggedIn(false)}>Logout</button>
+                        <h2>Welcome {state.username}!</h2>
+                        <button onClick={() => dispatch({type: "logOut"})}>Logout</button>
                     </>
                 ) : (
                     <div>
                         <form onSubmit={handleSumbit}>
                             <label>Username:</label>
-                            <input type='text' placeholder='Username' autoComplete='username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <input type='text' placeholder='Username' autoComplete='username' value={state.username} onChange={(e) => dispatch({
+                                type: "field",
+                                fieldName: "username",
+                                payload: e.target.value,
+                            })} />
                             <label>Password:</label>
-                            <input type='text' placeholder='Password' autoComplete='current-password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input type='password' placeholder='Password' autoComplete='current-password' value={state.password} onChange={(e) => dispatch({
+                                type: "field",
+                                fieldName: "password",
+                                payload: e.target.value,
+                            })} />
                             <button type='submit'>Login</button>
-                            <p>{error}</p>
+                            <p>{state.error}</p>
                         </form>
                     </div>
                 )}
