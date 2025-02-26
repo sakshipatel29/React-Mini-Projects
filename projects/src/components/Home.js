2d2025430c4d45a9d6b9c4a1f5155ef5ee8f38d1
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-26 15:21:17 -0600

    css update

--- projects/src/components/Home.js
+++ projects/src/components/Home.js
@@ -1,23 +1,26 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 
 export const Home = () => {
     const [user, setUser] = useContext(UserContext);
     return (
-        <div> 
-        <h2>{user}'s Home</h2>
-        <button onClick={() => setUser("Somu")}>Edit Name</button>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/recommendations">Recommendations</Link>
-        <Link to="/podcast">Podcast</Link>
-        <Link to="/stopwatch">Stop Watch</Link>
-        <Link to="/todotask">To Do Tasks</Link>
-        <Link to="/stateandprops">State And Props</Link>
-        <Link to="/reactform">React Dynamic Form</Link>
-        <Link to="/reactdnd">React Drag and Drop</Link>
-        <Link to="/usereducer">Use Reducer</Link>
+        <div className="home-container">
+            <h2>{user}'s Home</h2>
+            <button className="edit-button" onClick={() => setUser("Somu")}>Edit Name</button>
+            <div className="links-grid">
+                <Link to="/dashboard" className="nav-link">Dashboard</Link>
+                <Link to="/recommendations" className="nav-link">Recommendations</Link>
+                <Link to="/podcast" className="nav-link">Podcast</Link>
+                <Link to="/stopwatch" className="nav-link">Stop Watch</Link>
+                <Link to="/todotask" className="nav-link">To Do Tasks</Link>
+                <Link to="/stateandprops" className="nav-link">State And Props</Link>
+                <Link to="/reactform" className="nav-link">React Dynamic Form</Link>
+                <Link to="/reactdnd" className="nav-link">React Drag and Drop</Link>
+                <Link to="/usereducer" className="nav-link">Use Reducer</Link>
+            </div>
         </div>
     )
 }
